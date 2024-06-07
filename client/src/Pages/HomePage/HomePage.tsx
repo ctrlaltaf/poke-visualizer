f8c604e278c8db7f3e438ea025768450ad7ecc71
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-06-06 21:13:42 -0700

    Added API call functionality for pokemon stats

--- client/src/Pages/HomePage/HomePage.tsx
+++ client/src/Pages/HomePage/HomePage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface Props {}
 
-const HomePage = (props: Props) => {
+const HomePage: React.FC = (props: Props) => {
   return (
     <>
       <h1>Home Page</h1>
--- client/src/Pages/SeachPage/SearchPage.tsx
+++ client/src/Pages/SeachPage/SearchPage.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchPokemonStatsProcess } from "../../api/api";
+import { PokemonStats } from "../../api/ApiInterface";
 
 interface Props {}
 
-const SearchPage = (props: Props) => {
+const SearchPage: React.FC = (props: Props) => {
+  const [pokemonStats, setPokemonStats] = useState<PokemonStats | null>(null);
+
+  useEffect(() => {
+    const fetchPokemonStats = async () => {
+      try {
+        // Fetch Pokemon stats
+        const stats = await fetchPokemonStatsProcess();
+
+        setPokemonStats(stats);
+      } catch (error) {
+        // Handle errors
+      }
+    };
+
+    fetchPokemonStats();
+  }, []);
+
   return (
     <>
       <h1>Search Page</h1>
+      {pokemonStats && (
+        <ul>
+          <li>Name: {pokemonStats.name}</li>
+          <li>HP: {pokemonStats.hp}</li>
+          <li>Attack: {pokemonStats.attack}</li>
+          <li>Defence: {pokemonStats.defence}</li>
+          <li>Special Attack: {pokemonStats.specialAttack}</li>
+          <li>Special Defence: {pokemonStats.specialDefence}</li>
+          <li>Speed: {pokemonStats.speed}</li>
+        </ul>
+      )}
     </>
   );
 };
