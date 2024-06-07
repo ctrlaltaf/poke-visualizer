8c8675403f70aa08794901b1cafeea78be18ca53
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-06-06 19:42:30 -0700

    added routes for pages

---
+++ client/src/Pages/SeachPage/SearchPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const SearchPage = (props: Props) => {
+  return (
+    <>
+      <h1>Search Page</h1>
+    </>
+  );
+};
+
+export default SearchPage;
---
+++ client/src/Routes/Routes.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import HomePage from "../Pages/HomePage/HomePage";
+import SearchPage from "../Pages/SeachPage/SearchPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "search", element: <SearchPage /> },
+    ],
+  },
+]);
